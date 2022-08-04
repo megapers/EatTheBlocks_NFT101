2b1300aa0ef385c2ab2e0a0029e089ddb3f4c9d7
Timur Makimov <megapers@gmail.com>
2022-08-04 01:08:55 -0700

    Added ERC1155 and Yoshi metadata

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,13 +1,13 @@
 const { ethers } = require("hardhat");
 
 async function main() {
-  const SuperMarioWorld = await ethers.getContractFactory("SuperMarioWorldOZ");
-  const superMarioWorld = await SuperMarioWorld.deploy("SuperMarioWorldOZ", "SPRMOZ");
+  const SuperMarioWorld = await ethers.getContractFactory("SuperMarioWorldERC1155");
+  const superMarioWorld = await SuperMarioWorld.deploy("SuperMarioWorldERC1155", "SPRME");
 
   await superMarioWorld.deployed();
   console.log("Success! Contract was deployed to: ", superMarioWorld.address);
 
-  await superMarioWorld.mint("https://ipfs.io/ipfs/QmWZjtuJN47aVcQfqnkRXpWGqyerjdGejqhe4m2Nqt2EGn");
+  await superMarioWorld.mint(10, "https://ipfs.io/ipfs/QmbaFBpFBDAxw8JpBsxMpVVtTFSM6Yv4xYQDPAyqXz7uS3");
 
   console.log("NFT successfully minted");
 }
